3740ff67dfc1899282db0b8be3dbcfc1f28d0b9e
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-03 19:13:14 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styles';
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>Minhas Tarefas</S.Title>
+    </S.Wrapper>
+  );
+};
+
+export default Header;
