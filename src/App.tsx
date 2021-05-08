3d7c421487ae4d11b2973ea864a68e04786c23c6
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-08 20:50:00 -0300

    feat: add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import {ContextProvider} from './state/context'
 
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
 
+import ToDoContainer from './components/ToDoContainer';
+import Header from './components/Header';
+import ToDoHeader from './components/ToDoHeader';
+import Tasks from './components/Tasks';
+
 function App() {
   return (
       <ThemeProvider theme={theme}>
-        
+        <ContextProvider>
+          <Header/>
+          <ToDoContainer>
+            <ToDoHeader/>
+            <Tasks/>
+          </ToDoContainer>
+        </ContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
